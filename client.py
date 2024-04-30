d1b96ee690bacb1257031727928f7ce3914480fe
Adam Byrne <adamrbyrne@gmail.com>
2024-04-30 14:49:59 +0100

    Refactor file path option in client.py and generate client keys

--- client.py
+++ client.py
@@ -14,11 +14,11 @@ def cli():
 @click.option(
     "--out",
     type=click.Path(),
-    default="/my_path/my_keypair.pem",
-    help="File path to save the generated keys ",
-    required=True,
+    default="key.pem",
+    help="File path to save the generated keys",
 )
 def generate_keypair(out):
+
     try:
         if os.path.exists(out) or os.path.exists(out + ".pub"):
             raise FileExistsError(errno.EEXIST, os.strerror(errno.EEXIST), out)
@@ -31,10 +31,10 @@ def generate_keypair(out):
         with open(out + ".pub", "wb") as f:
             f.write(pubkey.save_pkcs1())
 
-        click.echo(f"Keypair generated and saved to '{out}'.")
-    except OSError as e:
+        click.echo(f"Keys generated and saved at {out} and {out}.pub")
+    except Exception as e:
         if e.errno == errno.EEXIST:
-            click.echo(f"Error: File '{out}' already exists.")
+            click.echo(f"Error: Key '{out}' already exists.")
         elif e.errno == errno.EACCES:
             click.echo(f"Error: Permission denied to write to '{out}'.")
         else:
