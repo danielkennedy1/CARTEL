4e7b6d652b58b3fb91e5b116d35d1f32ab33bc38
Adam Byrne <adamrbyrne@gmail.com>
2024-05-02 20:42:01 +0100

    init, select, send and whoami

--- client.py
+++ client.py
@@ -1,39 +1,19 @@
 import os
+import json
 from typing import Tuple
 
+import requests
 import click
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 
 
-@click.group()
-def cli():
-    pass
-
-
-def get_local_keys() -> Tuple[RSA.RsaKey, RSA.RsaKey]:
-    cartel_dir = os.path.join(os.path.expanduser("~"), ".cartel")
-    with open(os.path.join(cartel_dir, "key.pem"), "rb") as f:
-        private_key = RSA.import_key(f.read())
-        if private_key is None:
-            raise ValueError("Invalid private key")
-
-    with open(os.path.join(cartel_dir, "key.pem.pub"), "rb") as f:
-        public_key = RSA.import_key(f.read())
-        if public_key is None:
-            raise ValueError("Invalid public key")
-
-    return public_key, private_key
-
-
-def get_public_key_bytes_length(public_key) -> int:
-    key: bytes = public_key.public_key().export_key()
-    return len(key[key.index(b"\n") : key.rindex(b"\n")])
-
-
-def encrypt_file(file: str, public_key: RSA.RsaKey) -> bytes:
-    buffer_size = get_public_key_bytes_length(public_key) // 2
-    scrambler = PKCS1_OAEP.new(public_key)
+def encrypt_file(file: str, key: RSA.RsaKey) -> bytes:
+    buffer_size = get_key_bytes_length(key) // 2
+    scrambler = PKCS1_OAEP.new(key)
+    encrypted_chunks = []
 
     with open(file, "rb") as f:
         while True:
@@ -42,14 +22,56 @@ def encrypt_file(file: str, public_key: RSA.RsaKey) -> bytes:
                 break
 
             encrypted = scrambler.encrypt(to_encrypt)
-            click.echo(encrypted.hex().encode("utf-8"))
+            encrypted_chunks.append(encrypted)
 
         plaintext = f.read()
 
-        encryptor = PKCS1_OAEP.new(public_key)
-        ciphertext = encryptor.encrypt(plaintext)
+    encryptor = PKCS1_OAEP.new(key)
+    ciphertext = encryptor.encrypt(plaintext)
+    encrypted_chunks.append(ciphertext)
+    return b"".join(encrypted_chunks)
 
-        return ciphertext
+
+def get_state():
+    cartel_dir = os.path.join(os.path.expanduser("~"), ".cartel")
+    if not os.path.exists(os.path.join(cartel_dir, "state.json")):
+        with open(os.path.join(cartel_dir, "state.json"), "w") as f:
+            f.write("{}")
+
+    with open(os.path.join(cartel_dir, "state.json"), "r") as f:
+        return json.loads(f.read())
+
+
+def update_state(state):
+    cartel_dir = os.path.join(os.path.expanduser("~"), ".cartel")
+
+    with open(os.path.join(cartel_dir, "state.json"), "w") as f:
+        f.write(json.dumps(state))
+
+
+def get_local_keys(user: str) -> Tuple[RSA.RsaKey, RSA.RsaKey]:
+    cartel_dir = os.path.join(os.path.expanduser("~"), ".cartel")
+    with open(os.path.join(cartel_dir, f"{user}/key.pem"), "rb") as f:
+        private_key = RSA.import_key(f.read())
+        if private_key is None:
+            raise ValueError("Invalid private key")
+
+    with open(os.path.join(cartel_dir, f"{user}/key.pem.pub"), "rb") as f:
+        public_key = RSA.import_key(f.read())
+        if public_key is None:
+            raise ValueError("Invalid public key")
+
+    return public_key, private_key
+
+
+def get_key_bytes_length(public_key) -> int:
+    key: bytes = public_key.public_key().export_key()
+    return len(key[key.index(b"\n") : key.rindex(b"\n")])
+
+
+@click.group()
+def cli():
+    pass  # using subcommand pattern
 
 
 @cli.command(help="Generate your key pairs to access the cartel")
@@ -60,32 +82,111 @@ def init():
             os.mkdir(cartel_dir)
 
         username = click.prompt("Enter your desired username")
-        if not username or len(username) < 3:
-            click.echo("Username must be at least 3 characters long")
+        user_list = requests.get("http://127.0.0.1:5000/users").json()
+
+        if username in user_list:
+            click.echo(f"Username {username} already exists")
             return
 
         key = RSA.generate(3072)
 
-        with open(os.path.join(cartel_dir, "key.pem"), "wb") as f:
+        user = requests.put(
+            "http://127.0.0.1:5000/users",
+            json={
+                "name": username,
+                "public_key": key.publickey().export_key("PEM").decode("utf-8"),
+            },
+        )
+
+        if user.status_code != 200:
+            click.echo(f"Error: {user.text}")
+            return
+
+        # check if username already exists
+        if not os.path.exists(os.path.join(cartel_dir, username)):
+            os.mkdir(os.path.join(cartel_dir, username))
+
+        with open(os.path.join(cartel_dir, f"{username}/key.pem"), "wb") as f:
             f.write(key.export_key("PEM"))
 
-        with open(os.path.join(cartel_dir, "key.pem.pub"), "wb") as f:
+        with open(os.path.join(cartel_dir, f"{username}/key.pem.pub"), "wb") as f:
             f.write(key.publickey().export_key("PEM"))
 
-        click.echo(f"Keys generated and saved at {os.path.abspath(cartel_dir)}")
+        click.echo(
+            f"Keys generated and saved at {os.path.abspath(os.path.join(cartel_dir, username))}"
+        )
+
+        update_state({"user": username})
 
     except Exception as e:
         click.echo(f"Error: {str(e)}")
 
 
-@cli.command(help="Send a file to the cartel")
-@click.argument("file", type=click.Path(exists=True))
-@click.argument("to", type=str)
-def send(file, to):
-    _, privkey = get_local_keys()
+@cli.command(help="Select local user")
+def select():
+    cartel_dir = os.path.join(os.path.expanduser("~"), ".cartel")
+    users = requests.get("http://127.0.0.1:5000/users").json()
 
-    ciphertext = encrypt_file(file, privkey)
-    # encrypt with the public key of the recipient
+    for user in users:
+        if not os.path.exists(os.path.join(cartel_dir, user)):
+            click.echo(f"User {user} does not have keys generated")
+            continue
+
+        if not os.path.exists(os.path.join(cartel_dir, user, "key.pem")):
+            click.echo(f"User {user} does not have a private key")
+            continue
+
+        if not os.path.exists(os.path.join(cartel_dir, user, "key.pem.pub")):
+            click.echo(f"User {user} does not have a public key")
+            continue
+
+    click.echo("Available users:")
+    for user in users:
+        click.echo(f" - {user}")
+
+    desired_user = click.prompt("Select a local user")
+    update_state({"user": desired_user})
+
+
+@cli.command(help="Display the current user")
+def whoami():
+    click.echo(get_state()["user"])
+
+
+@cli.command(help="Share a file with the cartel")
+@click.argument("file_path", type=click.Path(exists=True))
+@click.argument("to", type=str)
+def send(file_path, to):
+    sender = get_state()["user"]
+    _, sender_privkey = get_local_keys(sender)  # get only sender's private key
+    ciphertext = encrypt_file(file_path, sender_privkey)
+    receiver = requests.post("http://127.0.0.1:5000/users", json={"name": to})
+
+    if receiver.status_code != 200:
+        click.echo(f"Error: {receiver.text}")
+        return
+
+    receiver = json.loads(receiver.content.decode("utf-8"))
+    receiver_pubkey = RSA.import_key(receiver["public_key"])
+    ciphertext = encrypt_file(file_path, receiver_pubkey)
+
+    # sign the message
+    h = SHA256.new(ciphertext)
+    signature = pkcs1_15.new(sender_privkey).sign(h)
+
+    message = requests.put(
+        "http://127.0.0.1:5000/messages",
+        json={
+            "sender": sender,
+            "recipient": to,
+            "message": ciphertext.hex(),
+            "signature": signature.hex(),
+        },
+    )
+
+    if message.status_code != 200:
+        click.echo(f"Error: {message.text}")
+        return
 
 
 if __name__ == "__main__":
