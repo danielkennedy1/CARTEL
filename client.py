10942113cb5e7d07f92fda046f5c8a953f6129cd
Daniel Kennedy <danielrkennedy03@gmail.com>
2024-05-05 01:16:05 +0100

    Inbox

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import click
 
 from narco.local import init, select, whoami
-from narco.comms import send, whois, narcos
+from narco.comms import send, whois, narcos, inbox
 
 @click.group()
 def cli():
@@ -16,6 +16,8 @@ cli.add_command(whoami) # Get the current user name
 cli.add_command(send) # Send a file to the cartel
 cli.add_command(whois) # Get details of a user in the cartel
 cli.add_command(narcos) # List all usernames in the cartel
+cli.add_command(inbox) # List my messages
+
 
 if __name__ == "__main__":
     cli()
--- narco/comms.py
+++ narco/comms.py
@@ -10,6 +10,9 @@ from narco.conf import CARTEL_URL
 from narco.encrypt import encrypt_file
 from narco.local import get_state, get_local_keys
 
+# TODO; cache read messages, only fetch new messages (could be done serverside or here)
+# TODO: Cache user_id(s)
+
 @click.command(help="Share a file with the cartel")
 @click.argument("file_path", type=click.Path(exists=True))
 @click.argument("to", type=str)
@@ -31,11 +34,18 @@ def send(file_path, to):
     hash = SHA256.new(ciphertext)
     signature = pkcs1_15.new(sender_privkey).sign(hash)
 
+    sender_user_id = get_user_id(sender)
+    if sender_user_id is None:
+        click.echo(f"Error: {sender} not found in the cartel")
+        return
+    receiver_user_id = receiver["id"]
+
+
     message = requests.put(
         f"{CARTEL_URL}/messages",
         json={
-            "sender": sender,
-            "recipient": to,
+            "sender": sender_user_id,
+            "recipient": receiver_user_id,
             "message": ciphertext.hex(),
             "signature": signature.hex(),
         },
@@ -64,3 +74,24 @@ def narcos():
     for user in response.json():
         click.echo(user)
 
+@click.command(help="List my messages")
+def inbox():
+    if get_state().get("user") is None:
+        click.echo("User not selected. Please run `select` to select a user or `init` to create one.")
+        return
+    user_id = get_user_id(get_state()["user"])
+    if user_id is None:
+        return
+    response = requests.post(f"{CARTEL_URL}/messages", json={"user_id": user_id})
+    if response.status_code != 200:
+        click.echo(f"Error: {response.text}")
+        return
+    for message in response.json():
+        click.echo(message)
+
+def get_user_id(username: str):
+    response = requests.post(f"{CARTEL_URL}/users", json={"name": username})
+    if response.status_code != 200:
+        click.echo(f"Error connecting to cartel: {response.text}")
+        return None
+    return response.json()["id"]
