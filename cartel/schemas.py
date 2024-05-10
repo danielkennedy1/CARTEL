48b2e5a2064c6ad3de56e4784196876c10da1a98
Adam Byrne <adamrbyrne@gmail.com>
2024-05-10 11:16:35 +0100

    chore: file split schemas for code hygiene reasons

---
+++ cartel/schemas.py
@@ -0,0 +1,10 @@
+from schema import Schema, And, Use
+
+valid_name_and_public_key = Schema(
+    {"name": And(str, len), "public_key": And(str, len)})
+valid_name = Schema({"name": And(str, len)})
+valid_id = Schema({"id": And(Use(int), lambda n:  0 <= n)})
+valid_user_id = Schema({"user_id": And(Use(int), lambda n:  0 <= n)})
+valid_new_message = Schema({"sender": And(Use(int), lambda n:  0 <= n), "recipient": And(
+    Use(int), lambda n:  0 <= n), "message": And(str, len), "signature": And(str, len), })
+valid_message_id = Schema({"message_id": And(Use(int), lambda n:  0 <= n)})
