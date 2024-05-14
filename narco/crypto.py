7ea60f91d840d14233e06c2f99e892d05b191ab0
Adam Byrne <adamrbyrne@gmail.com>
2024-05-14 11:29:15 +0100

    AES is AESing

--- narco/crypto.py
+++ narco/crypto.py
@@ -1,14 +1,23 @@
-from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import PKCS1_OAEP, AES
 from Crypto.PublicKey import RSA
+from Crypto.Util.Padding import pad, unpad
 
 
-def encrypt_file(file: str, key: RSA.RsaKey) -> bytes:
-    file_bytes = file_to_bytes(file)
-    return encrypt(file_bytes, key)
+def encrypt_file(file: str, passkey: bytes) -> bytes:
+    cipher = AES.new(passkey, AES.MODE_CBC)
+    with open(file, "rb") as file:
+        plaintext = file.read()
+    ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
+    return cipher.iv + ciphertext
 
-def file_to_bytes(file_path: str) -> bytes:
-    with open(file_path, "rb") as file:
-        return file.read()
+
+def decrypt(input: bytes, passkey: bytes) -> bytes:
+    iv = input[:16]
+    ciphertext = input[16:]
+    cipher = AES.new(passkey, AES.MODE_CBC, iv=iv)
+    plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    return plaintext
+
 
 def encrypt(input: bytes, key: RSA.RsaKey) -> bytes:
     cipher = PKCS1_OAEP.new(key)
@@ -17,19 +26,7 @@ def encrypt(input: bytes, key: RSA.RsaKey) -> bytes:
 
     encrypted_blocks = []
     for i in range(0, len(input), max_length):
-        block = input[i:i+max_length]
+        block = input[i : i + max_length]
         encrypted_blocks.append(cipher.encrypt(block))
 
     return b"".join(encrypted_blocks)
-
-def decrypt(input: bytes, key: RSA.RsaKey) -> bytes:
-    cipher = PKCS1_OAEP.new(key)
-
-    max_length = key.size_in_bytes()
-
-    decrypted_blocks = []
-    for i in range(0, len(input), max_length):
-        block = input[i:i+max_length]
-        decrypted_blocks.append(cipher.decrypt(block))
-
-    return b"".join(decrypted_blocks)
