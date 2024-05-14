9203ce8c8747c73dbc2127537af1d6f45f03ccd6
Adam Byrne <adamrbyrne@gmail.com>
2024-05-14 10:50:02 +0100

    Added passkey to server

--- cartel/messages.py
+++ cartel/messages.py
@@ -10,22 +10,34 @@ from cartel.users import verify_password
 
 # List messages: return all messages for user
 def list_messages(user_id: int):
-    result = session.execute(select(Message.id, Message.sender).where(Message.recipient == user_id)).scalars().all() 
+    result = (
+        session.execute(
+            select(Message.id, Message.sender).where(Message.recipient == user_id)
+        )
+        .scalars()
+        .all()
+    )
     if not result:
         return Response("No messages found", status=404)
-    return jsonify(result) 
+    return jsonify(result)
+
 
 # Get message: return message data by ID
 def get_message(id: int):
     message = session.query(Message).get(id)
     if not message:
         return Response("Message not found", status=404)
-    return jsonify({"id": message.id,
-                    "sender": message.sender,
-                    "recipient": message.recipient,
-                    "message": message.message,
-                    "signature": message.signature
-                    })
+    return jsonify(
+        {
+            "id": message.id,
+            "sender": message.sender,
+            "recipient": message.recipient,
+            "message": message.message,
+            "signature": message.signature,
+            "passkey": message.passkey,
+        }
+    )
+
 
 # Send message: create message, return created message data
 def new_message(data: dict):
@@ -35,9 +47,9 @@ def new_message(data: dict):
 
     if not session.query(User).get(data["recipient"]):
         return Response("Recipient not found", status=404)
-    
+
     # verify the signature
-    sender_pubkey = session.execute(select(User.public_key).where(User.id == data["sender"])).scalar() # type: ignore
+    sender_pubkey = session.execute(select(User.public_key).where(User.id == data["sender"])).scalar()  # type: ignore
 
     if not sender_pubkey:
         return Response("Sender ID is not found", status=404)
@@ -52,15 +64,21 @@ def new_message(data: dict):
         return Response(f"Signature is invalid {e}", status=400)
 
     message = Message(
-            sender=data["sender"],
-            recipient=data["recipient"],
-            message=data["message"],
-            signature=data["signature"])
+        sender=data["sender"],
+        recipient=data["recipient"],
+        message=data["message"],
+        signature=data["signature"],
+        passkey=data["passkey"],
+    )
     session.add(message)
     session.commit()
-    return jsonify({"id": message.id,
-                    "sender": message.sender,
-                    "recipient": message.recipient,
-                    "message": message.message,
-                    "signature": message.signature
-                    })
+    return jsonify(
+        {
+            "id": message.id,
+            "sender": message.sender,
+            "recipient": message.recipient,
+            "message": message.message,
+            "signature": message.signature,
+            "passkey": message.passkey,
+        }
+    )
